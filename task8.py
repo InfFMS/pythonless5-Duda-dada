6eb8281a6156e0dab670482c1d47b10a3f15a472
Дуда Ирина Дмитриевна <i.duda@main.nsu.ru>
2024-10-30 14:12:01 +0700

    8

--- task8.py
+++ task8.py
@@ -4,3 +4,26 @@
 # Пример: ввод N = 4
 # [12, 77, 5555, 97]
 # Вывод: [77, 5555]
+from random import randint
+
+N = int(input())
+A = [0] * N
+for i in range(N):
+    A[i] = str(randint(10, 100000))
+print(A)
+
+
+def res(a):
+    for i in range(len(a) - 1):
+        if a[i] != a[i + 1]:
+            return None
+    return int(a)
+
+
+lst = list(map(res, A))
+while True:
+    if None in lst:
+        lst.remove(None)
+    else:
+        break
+print(lst)
